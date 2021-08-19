27e224726c71d389e0d8cf0b354fd88a7aa242be
Jonathandika <jonathandika1718@gmail.com>
2021-08-19 21:18:06 +0800

    Job Addition

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,29 @@ skillsHeader.forEach((el) => {
         toggleSkills)
 })
 
+/*==================== QUALIFICATION  ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
+
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiper = new Swiper('.portfolio__container', {
